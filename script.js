d3921e92de78f94f8cab59abc6c5421b0e57792f
Rahul Chauhan <56860601+rahulchauhan04@users.noreply.github.com>
2024-05-03 23:41:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
+	let lowerS1 = s1.toLowerCase();
+    let lowerS2 = s2.toLowerCase();
+	return lowerS1.indexOf(lowerS2);
 }
 
 // Please do not change the code below
